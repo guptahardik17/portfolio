814bad7a27e2510ab1d5457d035d9eec86e3096b
guptahardik17 <guptahardik17@gmail.com>
2019-03-18 07:59:33 +0530

    Updates

---
+++ precache-manifest.00030fc5b845bfaebd110b774a787492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0df8de0bae2ffb92e016",
+    "url": "/portfolio/static/css/main.8a9dc176.chunk.css"
+  },
+  {
+    "revision": "0df8de0bae2ffb92e016",
+    "url": "/portfolio/static/js/main.e9e1fda4.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "027e5e700f65c7dbe475",
+    "url": "/portfolio/static/js/2.659081b0.chunk.js"
+  },
+  {
+    "revision": "1ad0364a3f9a43eaee8d48d64b596f14",
+    "url": "/portfolio/index.html"
+  }
+];
